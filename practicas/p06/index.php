6408fdfa88c6901f3f3143633472b07284254f3f
ulisestc <ulisestc27@gmail.com>
2025-02-05 20:32:17 -0600

    Se realizó el ejercicio 2 de la práctica 6, donde creamos una matríz de Nx3 de 3 números aleatorios hasta conseguir un impar -> par -> impar

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 6</title>
 </head>
 <body>
     <!-- <h2>Ejercicio 1</h2>
@@ -16,6 +16,9 @@
         
         echo '<hr><h2>Ejercicio 2</h2><p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una secuencia compuesta por: impar, par, impar</p>';
         // EJERCICIO 2
+        secuencia_aleatoria();
+
+        echo '<hr><h2>Ejercicio 3</h2><p>Utiliza un ciclo while para encontrar el primer número entero obtenido aleatoriamente, pero que además sea múltiplo de un número dado.</p>';
     ?>
 
 </body>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -5,27 +5,53 @@
             $num = $_GET['numero'];
 
             if(($num % $a) == 0 && ($num % $b) == 0){
-                echo '<br> El número '. $num . ' <b>SI</b> es múltiplo de ' . $a . ' y ' . $b . ' <br> ';
+                echo 'El número '. $num . ' <b>SI</b> es múltiplo de ' . $a . ' y ' . $b . ' <br> ';
             }
             else
             {
-                echo '<br> El número '. $num . ' <b>NO</b> es múltiplo de ' . $a . ' y ' . $b . ' <br> ';
+                echo 'El número '. $num . ' <b>NO</b> es múltiplo de ' . $a . ' y ' . $b . ' <br> ';
             }
         }
+        else{
+            echo 'para comenzar, teclea "?numero=X en la barra de direcciones!"';
+        }
+        unset($num);
+        unset($a);
+        unset($b);
+    }
+
+    function secuencia_aleatoria(){
+        // Inciar variables
+        $matriz = [];
+        $iteraciones = 0;
+        $numeros_generados = 0;
+        
+        do{
+            // Generación de los num aleatorios
+            $_1 = rand(0,10);
+            $_2 = rand(0,10);
+            $_3 = rand(0,10);
+            // Ingresar números a matríz
+            $matriz[] = [$_1, $_2, $_3];
+            $iteraciones++;
+            $numeros_generados += 3;
+        }
+        while(!((($_1 % 2) != 0) && (($_2 % 2) == 0) && (($_3 % 2) != 0)));
+        
+        // print_r($matriz);
+        for ($i = 0; $i < $iteraciones; $i++){
+            echo '<br>' . $matriz[$i][0] . ' ' . $matriz[$i][1] . ' ' . $matriz[$i][2];
+        }
+
+        echo '<br><br>' . $numeros_generados . ' numeros generados en ' . $iteraciones . ' iteraciones.<br>';
+
+        // unsets
+        unset($_1);
+        unset($_2);
+        unset($_3);
+        unset($matriz);
+        unset($iteraciones);
+        unset($numeros_generados);
     }
 ?>
 
-<!-- <?php
-        // if(isset($_GET['numero']))
-        // {
-        //     $num = $_GET['numero'];
-        //     if ($num%5==0 && $num%7==0)
-        //     {
-        //         echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-        //     }
-        //     else
-        //     {
-        //         echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-        //     }
-        // }
-    ?> -->
