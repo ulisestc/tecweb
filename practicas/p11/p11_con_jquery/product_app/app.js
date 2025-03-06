9245df2a4beae3df7c919276c9f5f1df8a77671c
ulisestc <ulisestc27@gmail.com>
2025-03-06 11:34:38 -0600

    p11 - Se agregaron los archivos base del ejercicio 3 (p11_con_jquery

---
+++ practicas/p11/p11_con_jquery/product_app/app.js
@@ -0,0 +1,21 @@
+// JSON BASE A MOSTRAR EN FORMULARIO
+var baseJSON = {
+    "precio": 0.0,
+    "unidades": 1,
+    "modelo": "XX-000",
+    "marca": "NA",
+    "detalles": "NA",
+    "imagen": "img/default.png"
+};
+
+function init() {
+    /**
+     * Convierte el JSON a string para poder mostrarlo
+     * ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
+     */
+    var JsonString = JSON.stringify(baseJSON,null,2);
+    document.getElementById("description").value = JsonString;
+
+    // SE LISTAN TODOS LOS PRODUCTOS
+    // listarProductos();
+}
